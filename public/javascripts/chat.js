625692a876a420143403eb151c6b3c3fc49262a4
SKSHARATH96 <sharath_sk@outlook.com>
2017-10-08 07:45:24 +0000

    ws not working

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,17 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
-$scope.messages = [{'sender': 'USER',
-                    'text': 'hi',
-                    'time': '10:01 AM'},
-                    {'sender': 'BOT',
-                      'text': 'hello, what can I help you with?',
-                       'time': '10:01 AM'},
-                   {'sender': 'USER',
-                     'text': 'news about dfgfbgyyn',
-                      'time': '10:01 AM'},
-                    {'sender': 'BOT',
-                     'text': 'INVALID SEARCH',
-                     'time': '10:01 AM'}
-                     ]
+$scope.messages = [];
+
+
+                     var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+                        exampleSocket.onmessage  =   function  (event) {
+                            var jsonData = JSON.parse(event.data);
+                            jsonData.time = new Date()
+                                .toLocaleTimeString();
+                            $scope.messages.push(jsonData);
+                            $scope.$apply();
+                            console.log(jsonData);
+                        };
+                        $scope.sendMessage = function () {
+                               exampleSocket.send($scope.userMessage);
+                               $scope.userMessage = "";
+                           };
+                           $scope.trust = $sce.trustAsHtml;$scope.trust = $sce.trustAsHtml;
 });
