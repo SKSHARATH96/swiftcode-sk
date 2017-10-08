6e2c059d4bcc411eb02bc14dd75ac4aae563b4dc
SKSHARATH96 <sharath_sk@outlook.com>
2017-10-08 05:51:51 +0000

    md-card

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,17 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+$scope.messages = [{'sender': 'USER',
+                    'text': 'hi',
+                    'time': '10:01 AM'},
+                    {'sender': 'BOT',
+                      'text': 'hello, what can I help you with?',
+                       'time': '10:01 AM'},
+                   {'sender': 'USER',
+                     'text': 'news about dfgfbgyyn',
+                      'time': '10:01 AM'},
+                    {'sender': 'BOT',
+                     'text': 'INVALID SEARCH',
+                     'time': '10:01 AM'}
+                     ]
 });
